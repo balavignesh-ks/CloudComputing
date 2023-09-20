4635955ee0958be14052927ebefcbc4228d67d18
balavignesh-ks <ksbalavignesh@gmail.com>
2023-09-20 17:09:18 +0100

    Problem 1 - Instance method to Class Method

--- CloudDevOpsSec/PythonProblems/PythonProblem1a.py
+++ CloudDevOpsSec/PythonProblems/PythonProblem1a.py
@@ -1,7 +1,17 @@
 class Person:
 
-    def __init__(self, Name):
-        self.full_Name = Name
+    full_Name=input("Enter your Name?\n")
+    
+#    def __init__(self, Name):
+#        self.full_Name = Name
 
-    def welcome_msg(self):
-        return "Hello {}, Welcome".format(self.full_Name)
+#    def welcome_msg(self):
+#        return "Hello {}, Welcome".format(self.full_Name)
+
+#    @staticmethod
+#    def welcome_msg(Name):
+#        return "Hello {}, Welcome".format(Name)
+
+    @classmethod
+    def welcome_msg(cls):
+        return "Hello {}, Welcome".format(cls.full_Name)
--- CloudDevOpsSec/PythonProblems/PythonProblem1b.py
+++ CloudDevOpsSec/PythonProblems/PythonProblem1b.py
@@ -1,7 +1,7 @@
 from PythonProblem1a import Person
 
-name = input("Please enter your Name?\n")
+#name = input("Please enter your Name?\n")
 
-p1 = Person(name)
+#p1 = Person(name)
 
-print(p1.welcome_msg())
+print(Person.welcome_msg())
