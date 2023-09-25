4409bc80dcbdb8dcd1456ce80951940b52cbf34e
balavignesh-ks <ksbalavignesh@gmail.com>
2023-09-25 17:16:18 +0100

    Fixing inheritance in Problem 3

--- CloudDevOpsSec/PythonProblems/PythonProblem3a.py
+++ CloudDevOpsSec/PythonProblems/PythonProblem3a.py
@@ -3,35 +3,36 @@ from enum import IntEnum
 class Employee:
     BaseSalary = float(1000)
 
-    def __init__(self,Name,Id,Type):
+    def __init__(self,Name,Id,Type,SalesMade):
         self.EmployeeName = Name
         self.EmployeeId = Id
         self.EmployeeType = Type
+        self.SalesMade = SalesMade
 
 class EmployeeType(IntEnum):
     CommissionEmployee = 1
     SalariedCommissionEmployee = 2
 
-class Sales(Employee):
-    def __init__(self,Name,Id,Type,Sales):
-        super().__init__(Name,Id,Type)
-        self.SalesMade = float(Sales)
-        self.Percentage_Sales_Made = float(percent(self.SalesMade))
-        
-    def get_salary(self):
-        if  self.EmployeeType == EmployeeType.CommissionEmployee:
-            self.Salary = float(self.Percentage_Sales_Made)
-        else:
-            self.Salary = float(self.Percentage_Sales_Made) + float(Employee.BaseSalary)
-        
-        return self.Salary
+class CommissionEmployee(Employee):
+    def __init__(self,Name,Id,Type,SalesMade):
+        super().__init__(Name,Id,Type,SalesMade)
     
+    def salary(self):
+        return float(percent(self.SalesMade))
+    
+class SalariedCommissionEmployee(CommissionEmployee):
+    def __init__(self,Name,Id,Type,SalesMade):
+        super().__init__(Name,Id,Type,SalesMade)
+
+    def salary(self):
+        return super().salary() + float(Employee.BaseSalary)
+   
 def percent(SalesMade):
     Percent = float((5*SalesMade)/100)
     return Percent
 
 def get_employee_type():
-    Type_response = input(f"Is the employee {(EmployeeType._member_names_)[0]} or {(EmployeeType._member_names_)[1]}? Enter '{EmployeeType.CommissionEmployee}' for {(EmployeeType._member_names_)[0]} or '{EmployeeType.SalariedCommissionEmployee}' for {(EmployeeType._member_names_)[1]}: ")
+    Type_response = input(f"Enter the Employee Type? Enter '{EmployeeType.CommissionEmployee}' for {(EmployeeType._member_names_)[0]} or '{EmployeeType.SalariedCommissionEmployee}' for {(EmployeeType._member_names_)[1]}: ")
     Type = EmployeeType(int(Type_response))
     return Type
 
--- CloudDevOpsSec/PythonProblems/PythonProblem3b.py
+++ CloudDevOpsSec/PythonProblems/PythonProblem3b.py
@@ -15,8 +15,11 @@ while check:
 
     SalesMade = float(input("Enter the amount in Sales Employee made?\n"))
 
-    e1 = Sales(Name,Id,Type,SalesMade)
-
-    print(f"\nSalary of Employee {e1.EmployeeName}[{e1.EmployeeId}] Type '{Type.name}' is {e1.get_salary()}\n\nPlease Note!!! As Per Company Policy, {(EmployeeType._member_names_)[0]} gets 5% of Commission from the sales they made & {(EmployeeType._member_names_)[1]} get Base_Salary plus 5% of Commission from the sales they made\n")
+    if Type == 1:
+        e1 = CommissionEmployee(Name,Id,Type,SalesMade)
+        print(f"\nSalary of Employee {e1.EmployeeName}[{e1.EmployeeId}] Type '{Type.name}' is {e1.salary()}\n\nPlease Note!!! As Per Company Policy, {(EmployeeType._member_names_)[0]} gets 5% of Commission from the sales they made & {(EmployeeType._member_names_)[1]} get Base_Salary plus 5% of Commission from the sales they made\n")
+    elif Type == 2:
+        e1 = SalariedCommissionEmployee(Name,Id,Type,SalesMade)
+        print(f"\nSalary of Employee {e1.EmployeeName}[{e1.EmployeeId}] Type '{Type.name}' is {e1.salary()}\n\nPlease Note!!! As Per Company Policy, {(EmployeeType._member_names_)[0]} gets 5% of Commission from the sales they made & {(EmployeeType._member_names_)[1]} get Base_Salary plus 5% of Commission from the sales they made\n")
 
     check = continue_check()
